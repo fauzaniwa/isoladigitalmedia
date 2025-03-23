33f762fc971e60594be617c1a82b9f4d84a54dec
EyeesL <miftahulfadli223@gmail.com>
2025-03-23 15:08:02 +0700

    fixed 2

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./.html", "./src//.{js,ts,jsx,tsx}"], // Pastikan path ini sesuai dengan proyekmu
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
